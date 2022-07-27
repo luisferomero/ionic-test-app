8f82c1fe631180142e2e78907e8e21e615335f83
Luis Romero <luisfe.rl@hotmail.com>
2022-07-27 16:39:24 -0400

    WIP

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,18 +4,28 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="home" />
+          <ion-label>Inicio</ion-label>
         </ion-tab-button>
-          
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+
+        <ion-tab-button tab="tab2" href="/tabs/stores">
+          <ion-icon :icon="cube" />
+          <ion-label>Tiendas</ion-label>
         </ion-tab-button>
-        
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+
+        <ion-tab-button tab="tab3" href="/tabs/search">
+          <ion-icon :icon="addCircle" />
+          <ion-label>Piezas</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/orders">
+          <ion-icon :icon="listCircle" />
+          <ion-label>Ordenes</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tabs" href="/tabs/chats">
+          <ion-icon :icon="chatbubbles" />
+          <ion-label>Chat</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -23,19 +33,49 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { defineComponent } from "vue";
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from "@ionic/vue";
+import {
+  ellipse,
+  square,
+  triangle,
+  home,
+  cube,
+  addCircle,
+  listCircle,
+  chatbubbles,
+} from "ionicons/icons";
 
 export default defineComponent({
-  name: 'TabsPage',
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  name: "TabsPage",
+  components: {
+    IonLabel,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonPage,
+    IonRouterOutlet,
+  },
   setup() {
     return {
-      ellipse, 
-      square, 
+      ellipse,
+      square,
       triangle,
-    }
-  }
+      home,
+      cube,
+      addCircle,
+      listCircle,
+      chatbubbles,
+    };
+  },
 });
 </script>
